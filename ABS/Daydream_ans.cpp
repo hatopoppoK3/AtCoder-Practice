c531152b0f25b1b2d5a71586c8726af165da424e
hatopoppoK3 <animeacount0923@gmail.com>
2019-05-16 18:31:23 +0900

    update 2019/5/16

---
+++ ABS/Daydream_ans.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main () {
+    string str;
+    string divide[] = {"dream", "dreamer", "erase", "eraser"};
+    cin >> str;
+
+    reverse(str.begin(), str.end());
+    for (int i = 0; i < 4; i++) {
+        reverse(divide[i].begin(), divide[i].end());
+    }
+
+    bool can = true;
+    for (int i = 0; i < str.size();) {
+        bool can2 = false;
+        for (int j = 0; j < 4; j++) {
+            string d = divide[j];
+            if (str.substr(i, d.size()) == d) {
+                can2 = true;
+                i += d.size();
+            }
+        }
+        if(!can2) {
+            can = false;
+            break;
+        }
+    }
+    if (can) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
