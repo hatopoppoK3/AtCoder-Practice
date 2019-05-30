296e7a66927fefe86ce534d34612dd1562d4ff56
hatopoppoK3 <animeacount0923@gmail.com>
2019-05-30 23:10:35 +0900

    update 2019/5/30

---
+++ ABC/127/Prison.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, M;
+    int L[100000], R[100000];
+    cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> L[i] >> R[i];
+    }
+    sort(L, L + M);
+    sort(R, R + M);
+    if (0 <= R[0] - L[M - 1]) {
+        cout << R[0] -L[M - 1] + 1 << endl;
+    }
+    else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
