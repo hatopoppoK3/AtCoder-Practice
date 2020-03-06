e7b217a2b74bec208e5878c05069e2a11f65986a
poppo <hatopoppo0320@gmail.com>
2020-03-06 20:54:17 +0900

    update 2020/03/06

--- ARC/103/C.py
+++ ARC/103/C.py
@@ -8,28 +8,51 @@ for i in range(0, N):
         Y[V[i]] += 1
     else:
         X[V[i]] += 1
-
-x = sorted(X.items(), reverse=True, key=lambda x: x[1])[:2]
-y = sorted(Y.items(), reverse=True, key=lambda x: x[1])[:2]
+if len(X) != 1:
+    x = sorted(X.items(), reverse=True, key=lambda x: x[1])[:2]
+else:
+    x = sorted(X.items(), reverse=True, key=lambda x: x[1])[:1]
+if len(Y) != 1:
+    y = sorted(Y.items(), reverse=True, key=lambda x: x[1])[:2]
+else:
+    y = sorted(Y.items(), reverse=True, key=lambda x: x[1])[:1]
 if x[0][0] == y[0][0]:
     ans = N
     tmp = 0
-    for i in range(0, N):
-        if i & 1:
-            if V[i] != y[1][0]:
-                tmp += 1
-        else:
-            if V[i] != x[0][0]:
-                tmp += 1
+    if len(y) == 1:
+        for i in range(0, N):
+            if i & 1:
+                if V[i] != y[0][0]+1:
+                    tmp += 1
+            else:
+                if V[i] != x[0][0]:
+                    tmp += 1
+    else:
+        for i in range(0, N):
+            if i & 1:
+                if V[i] != y[1][0]:
+                    tmp += 1
+            else:
+                if V[i] != x[0][0]:
+                    tmp += 1
     ans = min(ans, tmp)
     tmp = 0
-    for i in range(0, N):
-        if i & 1:
-            if V[i] != y[0][0]:
-                tmp += 1
-        else:
-            if V[i] != x[1][0]:
-                tmp += 1
+    if len(x) == 1:
+        for i in range(0, N):
+            if i & 1:
+                if V[i] != y[0][0]:
+                    tmp += 1
+            else:
+                if V[i] != x[0][0]+1:
+                    tmp += 1
+    else:
+        for i in range(0, N):
+            if i & 1:
+                if V[i] != y[0][0]:
+                    tmp += 1
+            else:
+                if V[i] != x[1][0]:
+                    tmp += 1
     ans = min(ans, tmp)
 else:
     ans = 0
