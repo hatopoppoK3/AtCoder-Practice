81f99fd0ca7c284d018f0ac94936ba66f06eb91c
poppo <hatopoppo0320@gmail.com>
2021-08-05 21:57:25 +0900

    update 2021/08/05

---
+++ ABC/172/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using ll = long long int;
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a * a * a + a * a + a << endl;
+
+    return 0;
+}
